47c617f9e8493672aabe4ec7f9b1f4cb02079c00
Wright <thewian27@gmail.com>
2025-06-21 17:27:53 +0300

    carousel

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,13 +1,17 @@
 import HeritageImage from "../assets/heritage-001.png"; 
 import UmojaImage from "../assets/umoja.png"; 
 import ArtBaselImage from "../assets/umoja-art-basel.jpg"; 
+import RhizomeImage from "../assets/exhibitions/rhizome-ny-2.jpeg";
+import InnerReflectionImage from "../assets/articles/inner-reflection/ir1.png";
 import { Link } from "react-router-dom";
+import "./Featured.css";
+
 
 export default function Featured () {
     return (
         <div className="featured">
             <h1>Featured...</h1>
-            <div className="row d-flex justify-content-center">
+            {/* <div className="row d-flex justify-content-center">
                 <div className="col-lg-8 featured-image-container"> 
                     <img src={HeritageImage} alt="" /> 
                     <div className="label">
@@ -18,8 +22,64 @@ export default function Featured () {
                             <a target="_blank" href="https://codecanvas.art/drop/D7brx6P5ASv6aYrgg7oMneNmT7y81Zqczwq1i7ksXKcv">View full collection on <b>Code Canvas</b></a> &nbsp;&nbsp;<i className="fa-solid fa-arrow-up-right-from-square"></i> </div>
                     </div>
                 </div>
+            </div> */}
+
+            <div id="featuredCarousel" className="carousel slide rounded overflow-hidden mb-5" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    
+                    <div className="carousel-item">
+                        <img src={RhizomeImage} className="d-block w-100 carousel-img" alt="Update 2" />
+                        <div className="carousel-overlay"></div>
+                        <div className="carousel-caption d-block d-md-block">
+                            <h5 >Rhizome World Exhibition</h5>
+                            <p>Umoja finally showcased at Rhizome event in New York, 2025.<br></br>
+                            <a target="_blank" href="/exhibitions">Learn more</a> &nbsp;&nbsp;<i className="fa-solid fa-arrow-up-right-from-square"></i>
+                            </p>
+                        </div>
+                    </div>
+    
+                    <div className="carousel-item">
+                        <img src={InnerReflectionImage} className="d-block w-100 carousel-img" alt="Update 2" />
+                        <div className="carousel-overlay"></div>
+                        <div className="carousel-caption d-block d-md-block">
+                            <h5>Inner Reflection</h5>
+                            <p>This is a <b>new</b> generative art release by Kamau Kamau and I, on <b>Bitcoin</b>. Check it out. 
+                            <br></br> 
+                            <a target="_blank" href="https://gamma.io/ordinals/collections/inner-reflection">Currently minting on <b>Gamma</b></a> &nbsp;&nbsp;<i className="fa-solid fa-arrow-up-right-from-square"></i>
+                            </p> 
+                        </div>
+                    </div>
+
+                    <div className="carousel-item active">
+                        <img src={HeritageImage} className="d-block w-100 carousel-img" alt="Update 1" />
+                        <div className="carousel-overlay"></div>
+                        <div className="carousel-caption d-block d-md-block">
+                            <h5>Heritage</h5>   
+                            <p>A curated generative art release on Solana (2024). 
+                                <br></br>  
+                                <a target="_blank" href="https://codecanvas.art/drop/D7brx6P5ASv6aYrgg7oMneNmT7y81Zqczwq1i7ksXKcv">View full collection on <b>Code Canvas</b></a> &nbsp;&nbsp;<i className="fa-solid fa-arrow-up-right-from-square"></i>
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* Add more slides as needed */}
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#featuredCarousel" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#featuredCarousel" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
+
+            <div className="featured-separator-2"></div>
             <div className="featured-separator"></div>
+
+            <h5>Quick Links...</h5>
+            <br></br>
+
             <div className="row d-flex justify-content-center"> 
 
                 <div className="col-lg-6 col-md-6 m-auto mb-3 d-flex justify-content-center"> 
